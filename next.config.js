d1f2bc78989b72d7184d9bee83321fb357a38fbe
wendy <thtlovelife@163.com>
2025-07-19 10:08:43 +0800

    fix: 移除basePath配置，修复GitHub Pages路由问题

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   distDir: 'docs',
-  basePath: process.env.NODE_ENV === 'production' ? '/JMA-Voting' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/JMA-Voting' : '',
   images: {
     unoptimized: true
   },
