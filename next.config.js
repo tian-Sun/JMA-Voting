5aa87d0c70a65989606121a770b11d37f8a8ac36
wendy <thtlovelife@163.com>
2025-07-19 10:54:32 +0800

    fix: 添加basePath和assetPrefix配置，修复GitHub Pages静态资源路径

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
   distDir: 'docs',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/JMA-Voting' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/JMA-Voting' : '',
   images: {
     unoptimized: true
   },
